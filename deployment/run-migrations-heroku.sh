d0bd69a8901b321b418fb33a0992ee0c6656c7ba
Unheppcat <unheppcat@yahoo.com>
2021-03-09 21:03:14 -0600

    Adds more environment refinements for Heroku
    Fixes a problem with TRUSTED_PROXIES

--- deployment/run-migrations-heroku.sh
+++ deployment/run-migrations-heroku.sh
@@ -2,4 +2,4 @@
 
 # Run all necessary database migrations on the remote heroku instance
 
-heroku run "bin/console doctrine:migrations:migrate -n"
+heroku run "bin/console doctrine:migrations:migrate -n; bin/console cache:clear"
