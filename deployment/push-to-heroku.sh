2433cd0e5a94b77833c118f2bd7d6a9f2124b008
Unheppcat <unheppcat@yahoo.com>
2021-03-12 23:53:22 -0600

    Updates composer.lock and a Heroku deployment utility

--- deployment/push-to-heroku.sh
+++ deployment/push-to-heroku.sh
@@ -2,4 +2,16 @@
 
 # This must be run from the project root directory, NOT the application directory!
 
+# Determine the containing directory of this script
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+# Change to the project root directory
+cd "$DIR/.." || exit
+
 git subtree push --prefix application heroku master
