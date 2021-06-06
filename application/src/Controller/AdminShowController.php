ace7a9ca15f7b62ba2473e8d0d24aaf4fda36434
Unheppcat <unheppcat@yahoo.com>
2021-06-06 16:47:59 -0500

    Adds paging and sorting for the Shows list page

--- application/src/Controller/AdminShowController.php
+++ application/src/Controller/AdminShowController.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpUndefinedClassInspection */
 
 namespace App\Controller;
 
@@ -7,6 +7,7 @@ use App\Form\ShowType;
 use App\Repository\ShowRepository;
 use App\Service\AnilistApi;
 use GuzzleHttp\Exception\GuzzleException;
+use JsonException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,14 +20,46 @@ class AdminShowController extends AbstractController
 {
     /**
      * @Route("/", name="admin_show_index", methods={"GET"})
+     * @param Request $request
      * @param ShowRepository $showRepository
      * @return Response
      */
-    public function index(ShowRepository $showRepository): Response
+    public function index(Request $request, ShowRepository $showRepository): Response
     {
+        $session = $request->getSession();
+        $currentPage = $session->get('page', 1);
+        $currentPerPage = $session->get('perPage', 5);
+        $currentSort = $session->get('sort', 'rumaji_asc');
+        $pageNum = $request->get('page', $currentPage);
+        $perPage = $request->get('perPage', $currentPerPage);
+        $sort = $request->get('sort', $currentSort);
+        switch($sort) {
+            case 'english_asc':
+                $sortColumn = 'english';
+                $sortOrder = 'ASC';
+                break;
+            case 'english_desc':
+                $sortColumn = 'english';
+                $sortOrder= 'DESC';
+                break;
+            case 'rumaji_desc':
+                $sortColumn = 'rumaji';
+                $sortOrder = 'DESC';
+                break;
+            default:
+                $sortColumn = 'rumaji';
+                $sortOrder = 'ASC';
+        }
+        $session->set('page', $pageNum);
+        $session->set('perPage', $perPage);
+        $session->set('sort', $sort);
+        $pagerfanta = $showRepository->getShowsSortedPaged($sortColumn, $sortOrder, $pageNum, $perPage);
+        $shows = $pagerfanta->getCurrentPageResults();
         return $this->render('show/index.html.twig', [
             'user' => $this->getUser(),
-            'shows' => $showRepository->getShowsSorted('romaji'),
+            'shows' => $shows,
+            'pager' => $pagerfanta,
+            'selectedSortName' => $sort,
         ]);
     }
 
@@ -35,7 +68,7 @@ class AdminShowController extends AbstractController
      * @param Request $request
      * @param AnilistApi $anilistApi
      * @return Response
-     * @throws GuzzleException
+     * @throws GuzzleException|JsonException
      */
     public function new(Request $request, AnilistApi $anilistApi): Response
     {
@@ -82,7 +115,7 @@ class AdminShowController extends AbstractController
      * @param Show $show
      * @param AnilistApi $anilistApi
      * @return Response
-     * @throws GuzzleException
+     * @throws GuzzleException|JsonException
      */
     public function edit(Request $request, Show $show, AnilistApi $anilistApi): Response
     {
--- application/src/Repository/ShowRepository.php
+++ application/src/Repository/ShowRepository.php
@@ -10,6 +10,8 @@ use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 
 /**
  * @method Show|null find($id, $lockMode = null, $lockVersion = null)
@@ -36,6 +38,20 @@ class ShowRepository extends ServiceEntityRepository
         $qb = $this->createQueryBuilder('s');
         $this->setOrderBy($qb, $sortColumn, $sortOrder);
         return $qb->getQuery()->getResult();
+    }
+
+    public function getShowsSortedPaged(
+        ?string $sortColumn = 'romaji',
+        ?string $sortOrder = 'ASC',
+        ?int $pageNum = 1,
+        ?int $perPage = 10
+    ): Pagerfanta {
+        $qb = $this->createQueryBuilder('s');
+        $this->setOrderBy($qb, $sortColumn, $sortOrder);
+        $pagerfanta = new Pagerfanta(new QueryAdapter($qb));
+        $pagerfanta->setMaxPerPage($perPage);
+        $pagerfanta->setCurrentPage($pageNum);
+        return $pagerfanta;
     }
 
     /**
@@ -120,9 +136,14 @@ class ShowRepository extends ServiceEntityRepository
             case 'none':
                 $orderBy = null;
                 break;
+            case 'rumaji':
+                $orderBy = 'japaneseTitle';
+                break;
             default:
                 $orderBy = 'japaneseTitle';
         }
+        // Prevent invalid sort orders
+        $sortOrder = (strtoupper($sortOrder) === 'DESC' ? 'DESC' : 'ASC');
         if ($orderBy !== null) {
             $qb->orderBy("s.{$orderBy}", $sortOrder);
         }
