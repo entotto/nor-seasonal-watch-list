3b480b3e439f4ab43b817b447353f01e850e0b82
entotto <52433044+entotto@users.noreply.github.com>
2024-04-16 22:48:18 -0800

    Add faker and doctrin-fixtures-bunder to dev dependencies

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -17,4 +17,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Demontpx\ParsedownBundle\DemontpxParsedownBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
